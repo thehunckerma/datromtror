7d658f4e299e4c873e1dd50fe8f446ec30389eab
SIDATE El Mahdi <thehunckerma@hotmail.com>
2020-10-31 23:02:24 +0100

    Add spring initializr generated boilerplate; Spring: Web, DevTools, Security | maven, JPA, Lambok

---
+++ src/main/java/com/ensak/datromtror/DatromtrorApplication.java
@@ -0,0 +1,13 @@
+package com.ensak.datromtror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatromtrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatromtrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensak/datromtror/DatromtrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensak.datromtror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatromtrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
